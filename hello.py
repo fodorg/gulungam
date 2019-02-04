f869526937fd1f660df29a0036972275978e52b7
manuel <sulmont.manuel@gmail.com>
2019-02-04 13:47:09 +0100

    hello worldj

--- hello.py
+++ hello.py
@@ -21,7 +21,9 @@ pygame.display.flip()
 #BOUCLE INFINIE
 
 continuer = 1
-
+a = -10;
+i = 1
+dir = 0; #dir = 1 > droite dir = -1 > gauche
 
 # BOUCLE D'ACCUEIL
 
@@ -29,15 +31,21 @@ while continuer:
     # Limitation de vitesse de la boucle
 
     pygame.time.Clock().tick(30)
-
+    dir = 0;
     for event in pygame.event.get():
         if event.type == QUIT:
             continuer = 0
 
         if event.type == KEYDOWN:
-            if event.key == K_SPACE:
+            if event.key == K_RIGHT:
                 perso.bondir(fenetre, fond);
 
+    perso.deplacer(a,dir)
+    a = a+i
+    if a < -10:
+        i = 1;
+    elif a > 10:
+        i = -1
     fenetre.blit(fond, (0, 0))
     fenetre.blit(perso.image,(perso.x, perso.y))  # dk.direction = l'image dans la bonne direction
     pygame.display.flip()
