4507df4aa68a39f2ee0fa5ea01af53eba63741aa
manuel <sulmont.manuel@gmail.com>
2019-02-04 14:11:08 +0100

    mario

--- hello.py
+++ hello.py
@@ -21,7 +21,8 @@ pygame.display.flip()
 #BOUCLE INFINIE
 
 continuer = 1
-a = -20;
+v = -20;
+a = 3;
 dir = 0; #dir = 1 > droite dir = -1 > gauche
 
 # BOUCLE D'ACCUEIL
@@ -44,10 +45,11 @@ while continuer:
             if event.key == K_RIGHT or event.key == K_LEFT:
                 dir = 0;
 
-    perso.deplacer(a,dir)
-    a = a+1
-    if a > 20:
-        a = -a
+    perso.deplacer(v,dir)
+    if v > 20:
+        v = -v
+    else :
+        v= v+a
     fenetre.blit(fond, (0, 0))
     fenetre.blit(perso.image,(perso.x, perso.y))  # dk.direction = l'image dans la bonne direction
     pygame.display.flip()
