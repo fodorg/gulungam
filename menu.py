a29c3f7e2aa6b791a2498dd5034f04e67e23cd49
fodorg <gergely-fodor@hotmail.fr>
2019-02-04 16:27:40 +0100

    menu creation

--- menu.py
+++ menu.py
@@ -1,1 +1,16 @@
+#Importation des bibliothèques nécessaires
 import pygame
+from pygame.locals import *
+
+pygame.init()
+
+fenetre = pygame.display.set_mode((1366, 768), RESIZABLE)
+
+
+continuer = 1
+
+while continuer:
+
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			continuer = 0
