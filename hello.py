3d22885d02ebd15032a523cafbfd74f6e6dbc9f4
fodorg <gergely-fodor@hotmail.fr>
2019-02-06 16:48:09 +0100

    end screen

--- hello.py
+++ hello.py
@@ -157,7 +157,7 @@ def game():
         fenetre.blit(text,text_rect)
 
         #affichage du timer
-        seconds = 5-(pygame.time.get_ticks() - start_ticks) / 1000  # calculate how many seconds
+        seconds = 180-(pygame.time.get_ticks() - start_ticks) / 1000  # calculate how many seconds
         min = int(seconds/60)
         seconds = int(seconds%60)
         if seconds <= 0 and min == 0:
@@ -179,6 +179,6 @@ def game():
 
 
 
-    return score
+    return int(score)
 
 
