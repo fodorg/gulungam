7d051c7363ef10cf028300e8f1e442a46c224c3a
manuel <sulmont.manuel@gmail.com>
2019-02-06 09:20:25 +0100

    colision fonctionne bien

--- init.py
+++ init.py
@@ -13,13 +13,14 @@ walls_list = pygame.sprite.Group()
 f = open("data/props.txt", "r")
 
 for line in f:
-  params = line.split(",")
-  if params[0] == "p":
-    plat = Platform(int(params[1]),int(params[2]),int(params[3]),int(params[4]))
-    platforms_list.add(plat)
-    all_sprite_list.add(plat)
-  elif params[0] == "w":
-    wall = Wall(int(params[1]),int(params[2]),int(params[3]),int(params[4]))
-    walls_list.add(wall)
-    all_sprite_list.add(wall)
+  if line[0] != '#':
+    params = line.split(",")
+    if params[0] == "p":
+      plat = Platform(int(params[1]),int(params[2]),int(params[3]),int(params[4]))
+      platforms_list.add(plat)
+      all_sprite_list.add(plat)
+    elif params[0] == "w":
+      wall = Wall(int(params[1]),int(params[2]),int(params[3]),int(params[4]))
+      walls_list.add(wall)
+      all_sprite_list.add(wall)
 #
